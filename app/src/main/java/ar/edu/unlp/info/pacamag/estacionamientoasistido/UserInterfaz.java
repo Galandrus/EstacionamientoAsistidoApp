81b297e4879aa9857902fbb15f2be917ba647cd6
Martin Galan <96martingalan@gmail.com>
2018-09-15 13:02:28 -0300

    Initial commit

---
+++ app/src/main/java/ar/edu/unlp/info/pacamag/estacionamientoasistido/UserInterfaz.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.pacamag.estacionamientoasistido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInterfaz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interfaz);
+    }
+}
